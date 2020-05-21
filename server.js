52ca7d85830774a37c2a4683dd3d7d805df173f9
yippptay <yippptay@gmail.com>
2020-05-21 09:49:25 -0700

    Ready to deploy.

--- server.js
+++ server.js
@@ -11,26 +11,30 @@ const db = mongoose.connection;
  **/
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3000;
-//___________________
-//Database
-//___________________
+/****
+ * Database
+ **/
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/project2";
-// Connect to Mongo
+/****
+ * Connect to mongo
+ **/
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
 });
-// Error / success
+/****
+ * Error / Success
+ **/
 db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
 db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
 db.on("disconnected", () => console.log("mongo disconnected"));
 // open the connection to mongo
 db.on("open", () => {});
-//___________________
-//Middleware
-//___________________
+/****
+ * Middleware
+ **/
 //use public folder for static assets
 app.use(express.static("public"));
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
@@ -38,14 +42,14 @@ app.use(express.urlencoded({ extended: false })); // extended: false - does not 
 app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
 //use method override
 app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
-//___________________
-// Routes
-//___________________
+/****
+ * Routes
+ **/
 //localhost:3000
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-//___________________
-//Listener
-//___________________
+/****
+ * Listener
+ **/
 app.listen(PORT, () => console.log("Listening on port:", PORT));
